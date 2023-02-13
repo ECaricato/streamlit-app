e7ce7008def68b819f9013384320946bb73b6613
ECaricato <46925635+ECaricato@users.noreply.github.com>
2023-02-13 09:55:55 +0100

    change index

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 streamlit.title("My Parents New Healthy Diner")
