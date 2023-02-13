407723a74a994b77887369c574db2a79c43e937d
ECaricato <46925635+ECaricato@users.noreply.github.com>
2023-02-13 09:56:53 +0100

    add preselection

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('Build Your Own Fruit Smoothie')
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
