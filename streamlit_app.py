a751c34636c004e1df59acc9d408262546403265
ECaricato <46925635+ECaricato@users.noreply.github.com>
2023-02-13 09:51:05 +0100

    Update streamlit_app.py
    add new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,5 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('Build Your Own Fruit Smoothie')
