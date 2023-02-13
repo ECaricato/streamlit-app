3f561282628f258d1eb2c938dad9cc5e3993f56c
ECaricato <46925635+ECaricato@users.noreply.github.com>
2023-02-13 09:54:04 +0100

    add content from csv

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -8,3 +12,4 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('Build Your Own Fruit Smoothie')
+streamlit.dataframe(my_fruit_list)
