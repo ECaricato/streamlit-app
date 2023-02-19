7670a0382eb1288fc9e38e2b07c1a4de46953a3e
ECaricato <46925635+ECaricato@users.noreply.github.com>
2023-02-19 13:40:15 +0100

    add snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
