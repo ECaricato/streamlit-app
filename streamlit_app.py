7d3fccfecca2d071e7203a4afc1f450467f5c9ae
ECaricato <46925635+ECaricato@users.noreply.github.com>
2023-02-19 13:06:23 +0100

    remove raw json

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,6 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
