72f4257c1ac06185d1403494a8b0ae89a4ce3f39
ECaricato <46925635+ECaricato@users.noreply.github.com>
2023-02-19 14:30:11 +0100

    add colon

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ def get_fruit_load_list():
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
 
-if streamlit.button('Get Fruit Load List')
+if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
